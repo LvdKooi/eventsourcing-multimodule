6caa14520816560c37bdb7c7a4115460ecbdd785
lvdkooi <lvvdkooi@hotmail.com>
2023-09-22 19:43:07 +0200

    moving around dependencies in order to make them all point inwards

--- core/src/main/java/nl/cjib/eventsourcing/usecases/BepaalFinancieleVerplichtingUseCaseImpl.java
+++ core/src/main/java/nl/cjib/eventsourcing/usecases/BepaalFinancieleVerplichtingUseCaseImpl.java
@@ -1,26 +1,24 @@
 package nl.cjib.eventsourcing.usecases;
 
 import lombok.RequiredArgsConstructor;
-import nl.cjib.eventsourcing.domain.aggregator.aggregate.FinancieleVerplichting;
-import nl.cjib.eventsourcing.domain.event.Event;
-import nl.cjib.eventsourcing.domain.mapper.EventMapper;
-import nl.cjib.eventsourcing.exception.NotFoundException;
-import nl.cjib.eventsourcing.dao.EventDAO;
 import nl.cjib.eventsourcing.command.BepaalFinancieleVerplichtingCommand;
 import nl.cjib.eventsourcing.domain.aggregator.FinancieleVerplichtingAggregator;
+import nl.cjib.eventsourcing.domain.aggregator.aggregate.FinancieleVerplichting;
+import nl.cjib.eventsourcing.domain.dao.EventDAO;
+import nl.cjib.eventsourcing.domain.event.Event;
+import nl.cjib.eventsourcing.exception.NotFoundException;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class BepaalFinancieleVerplichtingUseCaseImpl implements BepaalFinancieleVerplichtingUseCase {
 
     private final EventDAO eventDAO;
-    private final EventMapper mapper;
+
 
     public FinancieleVerplichting handle(BepaalFinancieleVerplichtingCommand command) {
         return Optional.ofNullable(command)
@@ -31,10 +29,7 @@ public class BepaalFinancieleVerplichtingUseCaseImpl implements BepaalFinanciele
     }
 
     private Set<Event> getEvents(BepaalFinancieleVerplichtingCommand command) {
-        return eventDAO.getEventsForVerplichtingnummer(command.verplichtingnummer())
-                .stream()
-                .map(mapper::map)
-                .collect(Collectors.toSet());
+        return eventDAO.getEventsForVerplichtingnummer(command.verplichtingnummer());
     }
 
     private static Predicate<Set<Event>> notEmpty() {
--- infra/src/main/java/module-info.java
+++ infra/src/main/java/module-info.java
@@ -4,6 +4,8 @@ module nl.cjib.eventsourcing.infra {
     requires org.hibernate.orm.core;
     requires spring.data.jpa;
     requires spring.context;
+    requires nl.cjib.eventsourcing.core;
+    exports nl.cjib.eventsourcing.mapper to spring.beans;
     exports nl.cjib.eventsourcing.entity to nl.cjib.eventsourcing.core;
     opens nl.cjib.eventsourcing.entity to org.hibernate.orm.core, spring.core;
     exports nl.cjib.eventsourcing.dao to nl.cjib.eventsourcing.core, spring.beans;
