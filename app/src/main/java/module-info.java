6531729342c017e29093e84869ef5ae76b5792ca
lvdkooi <lvvdkooi@hotmail.com>
2023-09-22 19:24:32 +0200

    refactoring module info

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -1,4 +1,11 @@
 open module nl.cjib.eventsourcing.app {
     requires spring.boot.autoconfigure;
     requires spring.boot;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires spring.data.jpa;
+    requires spring.context;
+    requires spring.beans;
+    requires spring.web;
+    requires spring.core;
 }
