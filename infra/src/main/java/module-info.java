3a3449adbc3bb3c076a57ec835c81892e0ab7b7d
lvdkooi <lvvdkooi@hotmail.com>
2023-09-22 19:44:23 +0200

    refactoring

--- infra/src/main/java/module-info.java
+++ infra/src/main/java/module-info.java
@@ -5,6 +5,7 @@ module nl.cjib.eventsourcing.infra {
     requires spring.data.jpa;
     requires spring.context;
     requires nl.cjib.eventsourcing.core;
+    requires org.mapstruct;
     exports nl.cjib.eventsourcing.mapper to spring.beans;
     exports nl.cjib.eventsourcing.entity to nl.cjib.eventsourcing.core;
     opens nl.cjib.eventsourcing.entity to org.hibernate.orm.core, spring.core;
